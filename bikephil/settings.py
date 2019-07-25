25a9d1849f3d401169191d938c46b9e59ce15e61
egfrank <egfrank@umd.edu>
2019-07-25 15:38:11 -0400

    update ALLOWED HOSTS

--- bikephil/settings.py
+++ bikephil/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('BIKEPHIL_SECRET_KEY')
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bike-philly.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
